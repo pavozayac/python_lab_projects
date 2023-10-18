01bc5e82249c70de041bf87a670f9aba26a5f21a
Paweł Zając <pavlzayac@gmail.com>
2023-10-18 11:26:42 +0200

    Add assignment no. 3

---
+++ lab3/time_spent.py
@@ -0,0 +1,26 @@
+time_map: dict[str, list[int]] = dict()
+
+cmd = input('Please input the command: a for adding an activity time, s for showing activities, t for top 3 and e for exit.\n')
+
+while cmd != 'e':
+    match cmd:
+        case 'a':
+            name: str = input('Activity name: ')
+            time: int = int(input('Time spent: '))
+
+            if name in time_map:
+                time_map[name].append(time)
+            else:
+                time_map[name] = [time]
+        case 's':
+            for key, item in time_map.items():
+                print(key + ': ' + str(item))
+        case 't':
+            top3 = sorted(time_map.items(),
+                          key=lambda i: sum(i[1]), reverse=True)[0:3]
+
+            for key, item in top3:
+                print(key + ': ' + str(item))
+
+    cmd = input(
+        'Please input the command: a for adding an activity time, s for showing activities, t for top 3 and e for exit.\n')
